a5fb397806f335a1caaaa7b57ce82154d4948462
Yanushkevich, Chen <Chen_Yanushkevich2@comcast.com>
2020-04-22 19:19:08 -0400

    express

---
+++ server.js
@@ -0,0 +1,10 @@
+﻿const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
+
+app.use(express.static(__dirname))
+app.use(express.static(path.join(__dirname, 'build')))
+// create a GET route
+app.get('/*', function (req, res) { res.sendFile(path.join(__dirname, 'build', 'index.html')); });
+app.listen(port)
