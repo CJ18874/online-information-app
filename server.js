a2691a5a5eabfac340608692eefad1b5ac08e3ad
Yanushkevich, Chen <Chen_Yanushkevich2@comcast.com>
2020-04-22 20:20:09 -0400

    procfile

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 ﻿const express = require('express');
+const compression = require('compression');
+const morgan = require('morgan');
 const path = require('path');
+// if we dont have a port then use 8080
 const port = process.env.PORT || 8080;
+
 const app = express();
+const dev = app.get('env');
+if (!dev) {
+    app.use(compression())
+    app.use(morgan('common'))
+}
 
 app.use(express.static(__dirname))
 app.use(express.static(path.join(__dirname, 'build')))
 // create a GET route
 app.get('/*', function (req, res) { res.sendFile(path.join(__dirname, 'build', 'index.html')); });
-app.listen(port)
+app.listen(port, () => console.log(`Start listening at http://localhost:${port}`))
